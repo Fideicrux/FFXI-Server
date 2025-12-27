854d0319aca19fd785f93e70856381ab91c1a597
FiniteGamer17 <caefiller@gmail.com>
2025-12-27 00:56:44 -0600

    Aggressor done.

--- scripts/effects/aggressor.lua
+++ scripts/effects/aggressor.lua
@@ -7,9 +7,14 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
     local jpLevel = target:getJobPointLevel(xi.jp.AGGRESSOR_EFFECT)
 
+    --Get Warrior Level
+    local warriorLevel = player:getMainJob() == xi.job.WAR and player:getMainLvl() or 0
+    local levelScale = math.floor((warriorLevel - 25) / 5)
+
+    --effects
     effect:addMod(xi.mod.RACC, effect:getPower() + jpLevel)
-    effect:addMod(xi.mod.ACCP, 15 + jpLevel)
-    effect:addMod(xi.mod.RACCP, 15)
+    effect:addMod(xi.mod.ACCP, 15 + levelScale + jpLevel)
+    effect:addMod(xi.mod.RACCP, 15 + levelScale)
     effect:addMod(xi.mod.EVA, -15)
 end
 
