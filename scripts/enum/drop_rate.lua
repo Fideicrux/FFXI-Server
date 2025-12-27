939def25ec1f0d4fa9607ed89e842c4deb88a5a1
FiniteGamer17 <caefiller@gmail.com>
2025-12-26 23:48:24 -0600

    Drop Rate Buff (works?)

--- scripts/enum/drop_rate.lua
+++ scripts/enum/drop_rate.lua
@@ -7,12 +7,12 @@ xi = xi or {}
 xi.drop_rate =
 {
     NEVER       = 0, --   0.00%
-    ULTRA_RARE  = 1, --   0.10%
-    SUPER_RARE  = 2, --   0.50%
-    VERY_RARE   = 3, --   1.00%
-    RARE        = 4, --   5.00%
-    UNCOMMON    = 5, --  10.00%
-    COMMON      = 6, --  15.00%
-    VERY_COMMON = 7, --  24.00%
+    ULTRA_RARE  = 1, --   1.00%
+    SUPER_RARE  = 2, --   2.50%
+    VERY_RARE   = 3, --   5.00%
+    RARE        = 4, --   10.00%
+    UNCOMMON    = 5, --  25.00%
+    COMMON      = 6, --  50.00%
+    VERY_COMMON = 7, --  75.00%
     GUARANTEED  = 8, -- 100.00%
 }
