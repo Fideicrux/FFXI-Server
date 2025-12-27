d934e5b245c7641fe974e113147ae20965da8366
Brandon Filler <blfiller@gmail.com>
2025-12-27 09:56:43 -0600

    Fixed aggressor. Function(target,effect) does not work with player:getMainJob(). Fixed to target:getMainJob(). ACCP and RACCP do not exist changed to ACC and RACC until new formula is made

--- scripts/effects/aggressor.lua
+++ scripts/effects/aggressor.lua
@@ -8,13 +8,13 @@ effectObject.onEffectGain = function(target, effect)
     local jpLevel = target:getJobPointLevel(xi.jp.AGGRESSOR_EFFECT)
 
     --Get Warrior Level
-    local warriorLevel = player:getMainJob() == xi.job.WAR and player:getMainLvl() or 0
+    local warriorLevel = target:getMainJob() == xi.job.WAR and target:getMainLvl() or 0
     local levelScale = math.floor((warriorLevel - 25) / 5)
 
     --effects
     effect:addMod(xi.mod.RACC, effect:getPower() + jpLevel)
-    effect:addMod(xi.mod.ACCP, 15 + utils.clamp(levelScale, 0, 10) + jpLevel)
-    effect:addMod(xi.mod.RACCP, 15 + utils.clamp(levelScale, 0, 10))
+    effect:addMod(xi.mod.ACC, 15 + utils.clamp(levelScale, 0, 10) + jpLevel)
+    effect:addMod(xi.mod.RACC, 15 + utils.clamp(levelScale, 0, 10))
     effect:addMod(xi.mod.EVA, -15)
 end
 
