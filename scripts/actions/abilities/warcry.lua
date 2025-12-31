b1e2ad28b4b914d437de277c1dce8e7054e51b79
Brandon Filler <blfiller@gmail.com>
2025-12-30 18:54:16 -0600

    changed warcry error message to unable to use JA

--- scripts/actions/abilities/warcry.lua
+++ scripts/actions/abilities/warcry.lua
@@ -7,7 +7,7 @@ local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
     if not player:hasStatusEffect(xi.effect.BERSERK) then
-        return xi.msg.basic.STATUS_PREVENTS, xi.effect.BERSERK
+        return xi.msg.basic.UNABLE_TO_USE_JA, 0
     end
     return 0, 0
 end
