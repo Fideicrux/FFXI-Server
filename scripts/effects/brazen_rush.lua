87046aa7f1386213ae2bd722e104d8e7d2f2c462
FiniteGamer17 <caefiller@gmail.com>
2026-01-03 02:22:09 -0600

    brazen ta change

--- scripts/effects/brazen_rush.lua
+++ scripts/effects/brazen_rush.lua
@@ -6,23 +6,26 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     local jpLevel = target:getJobPointLevel(xi.jp.BRAZEN_RUSH_EFFECT)
+    local drate = target:getMod(xi.mod.DOUBLE_ATTACK)
 
+    target:addMod(xi.mod.TRIPLE_ATTACK, drate)
     target:addMod(xi.mod.ATT, 4 * jpLevel)
     target:addMod(xi.mod.DOUBLE_ATTACK, effect:getPower())
 end
 
 effectObject.onEffectTick = function(target, effect)
-    local prevPower = effect:getPower()
-    local nextPower = prevPower - 10
+    --local prevPower = effect:getPower()
+    --local nextPower = prevPower - 10
 
-    target:delMod(xi.mod.DOUBLE_ATTACK, prevPower)
-    effect:setPower(nextPower)
-    target:addMod(xi.mod.DOUBLE_ATTACK, nextPower)
+    --target:delMod(xi.mod.DOUBLE_ATTACK, prevPower)
+    --effect:setPower(nextPower)
+    --ddtarget:addMod(xi.mod.DOUBLE_ATTACK, nextPower)
 end
 
 effectObject.onEffectLose = function(target, effect)
     local jpLevel = target:getJobPointLevel(xi.jp.BRAZEN_RUSH_EFFECT)
 
+    target:delMod(xi.mod.TRIPLE_ATTACK, drate)
     target:delMod(xi.mod.ATT, 4 * jpLevel)
     target:delMod(xi.mod.DOUBLE_ATTACK, effect:getPower())
 end
