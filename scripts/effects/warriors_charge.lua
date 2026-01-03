6b4f96dbc570b69bd03dccafc3ac2c7bf148b2b2
FiniteGamer17 <caefiller@gmail.com>
2026-01-03 02:16:10 -0600

    wc add TA

--- scripts/effects/warriors_charge.lua
+++ scripts/effects/warriors_charge.lua
@@ -5,6 +5,9 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
+    local drate = target:getMod(xi.mod.DOUBLE_ATTACK)
+
+    target:addMod(xi.mod.TRIPLE_ATTACK, drate)
     target:addMod(xi.mod.TRIPLE_ATTACK, effect:getPower())
     target:addMod(xi.mod.DOUBLE_ATTACK, 100)
 end
@@ -13,6 +16,7 @@ effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.TRIPLE_ATTACK, drate)
     target:delMod(xi.mod.TRIPLE_ATTACK, effect:getPower())
     target:delMod(xi.mod.DOUBLE_ATTACK, 100)
 end
