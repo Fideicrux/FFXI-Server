9b6a2ad432ad5beefd42232e69b7c52606d5a0e9
FiniteGamer17 <caefiller@gmail.com>
2026-01-06 12:41:43 -0600

    why effect even an option

--- scripts/effects/focus.lua
+++ scripts/effects/focus.lua
@@ -8,7 +8,7 @@ local effectObject = {}
 -- TODO: implement focus ranged accuracy bonus (needs verification)
 effectObject.onEffectGain = function(target, effect)
     local bonusPower = effect:getPower()
-    local monkLevel = (utils.getActiveJobLevel(target, xi.job.MNK) + 1) * 1.32
+    local monkLevel =(utils.getActiveJobLevel(target, xi.job.MNK) + 1) * 1.32
 
     -- https://wiki.ffo.jp/html/2841.html
     effect:addMod(xi.mod.ACC, monkLevel + bonusPower)
@@ -16,14 +16,14 @@ effectObject.onEffectGain = function(target, effect)
     -- https://www.bg-wiki.com/ffxi/Focus
     effect:addMod(xi.mod.CRITHITRATE, math.floor(monkLevel * 0.2))
 
-    target:addMod(xi.SUBTLE_BLOW_EFFECT, effect:getSubPower())
+    target:addMod(xi.SUBTLE_BLOW, effect:getSubPower())
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    target:delMod(xi.SUBTLE_BLOW_EFFECT, effect:getSubPower())
+    target:delMod(xi.SUBTLE_BLOW, effect:getSubPower())
 end
 
 return effectObject
