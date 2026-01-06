4d4b758f9624294d09d102bf491a5b131b211da2
FiniteGamer17 <caefiller@gmail.com>
2026-01-06 04:38:54 -0600

    consume mana not suck while here

--- scripts/globals/combat/damage_additions.lua
+++ scripts/globals/combat/damage_additions.lua
@@ -40,8 +40,8 @@ xi.combat.damage.consumeManaAddition = function(actor)
         return 0
     end
 
-    local bonusDamage = math.floor(actor:getMP() / 10)
-    actor:setMP(0)
+    local bonusDamage = math.floor(actor:getMP() / 2)
+    actor:delMP(bonusDamage)
     actor:delStatusEffect(xi.effect.CONSUME_MANA)
 
     return bonusDamage
