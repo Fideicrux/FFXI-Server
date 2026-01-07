60bcca3dcb06f0e6c4c2d500db903050429f51a6
FiniteGamer17 <caefiller@gmail.com>
2026-01-07 13:23:41 -0600

    stupid ass auto fixer

--- scripts/effects/focus.lua
+++ scripts/effects/focus.lua
@@ -17,7 +17,7 @@ effectObject.onEffectGain = function(target, effect)
     -- https://www.bg-wiki.com/ffxi/Focus
     effect:addMod(xi.mod.CRITHITRATE, math.floor(monkLevel * 0.2))
 
-    effect:addMod(xi.SUBTLE_BLOW, merits)
+    effect:addMod(xi.mod.SUBTLE_BLOW, merits)
 end
 
 effectObject.onEffectTick = function(target, effect)
