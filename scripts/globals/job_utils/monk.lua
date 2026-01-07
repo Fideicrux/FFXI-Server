369145a02c3e7e2129494375b1bacc4b574d840b
FiniteGamer17 <caefiller@gmail.com>
2026-01-06 22:08:56 -0600

    chi blast buff

--- scripts/globals/job_utils/monk.lua
+++ scripts/globals/job_utils/monk.lua
@@ -87,7 +87,7 @@ xi.job_utils.monk.useChiBlast = function(player, target, ability)
     end
 
 
-    local base = player:getStat(xi.mod.VIT) * (0.5 + (math.random() / 2))
+    local base = player:getStat(xi.mod.VIT)
     local dmg = math.floor(base * multiplier)
 
     dmg = xi.ability.adjustDamage(dmg, player, ability, target, xi.attackType.BREATH, xi.damageType.ELEMENTAL, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
