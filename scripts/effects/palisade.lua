7cd12beca74d57d017e0df7a0dd89005bbe1139c
FiniteGamer17 <caefiller@gmail.com>
2026-01-08 13:47:12 -0600

    range to palisade

--- scripts/effects/palisade.lua
+++ scripts/effects/palisade.lua
@@ -7,6 +7,7 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.PALISADE_BLOCK_BONUS, effect:getPower())
     target:addMod(xi.mod.UDMGPHYS, (effect:getPower() * -100))
+    target:addMod(xi.mod.UDMGRANGE, (effect:getPower() * -100))
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -15,6 +16,7 @@ end
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.PALISADE_BLOCK_BONUS, effect:getPower())
     target:delMod(xi.mod.UDMGPHYS, (effect:getPower() * -100))
+    target:delMod(xi.mod.UDMGRANGE, (effect:getPower() * -100))
 
 end
 
