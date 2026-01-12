b5ac625bc2314a6e34592004091ef2d22e965922
FiniteGamer17 <caefiller@gmail.com>
2026-01-11 20:15:43 -0600

    combined nether void and dark seal into the same addeffect

--- scripts/effects/nether_void.lua
+++ scripts/effects/nether_void.lua
@@ -5,6 +5,15 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
+    local power    = player:getMerit(xi.merit.DARK_SEAL) 
+    local subPower = player:getMerit(xi.merit.DARK_SEAL) * player:getMod(xi.mod.ENHANCES_DARK_SEAL) / 10
+
+    target:delStatusEffectSilent(xi.effect.DIVINE_EMBLEM)
+    target:delStatusEffectSilent(xi.effect.DIVINE_SEAL)
+    target:delStatusEffectSilent(xi.effect.ELEMENTAL_SEAL)
+
+    effect:addMod(xi.mod.DARK_MAGIC_CAST, -power)
+    effect:addMod(xi.mod.DARK_MAGIC_DURATION, subPower)
 end
 
 effectObject.onEffectTick = function(target, effect)
--- scripts/globals/job_utils/dark_knight.lua
+++ scripts/globals/job_utils/dark_knight.lua
@@ -96,10 +96,6 @@ end
 xi.job_utils.dark_knight.useDarkSeal = function(player, target, ability)
     -- Power: Each merit level after the first reduces Dark Magic casting time by -10% (total of -40% bonus).
     -- Sub Power: Enhances Dark Seal effect by increasing duration of Dark Magic by 10% per merit level (total of 50% bonus).
-    local power    = player:getMerit(xi.merit.DARK_SEAL) 
-    local subPower = player:getMerit(xi.merit.DARK_SEAL) * player:getMod(xi.mod.ENHANCES_DARK_SEAL) / 10
-
-    player:addStatusEffect(xi.effect.DARK_SEAL, power, 0, 60, 0, subPower)
     player:addStatusEffect(xi.effect.NETHER_VOID, 100, 0, 60)
 
     return xi.effect.DARK_SEAL
