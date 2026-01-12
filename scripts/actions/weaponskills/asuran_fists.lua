8ebdc517d1f3484f0240d663c28fb9c6e2d4ea58
Brandon Filler <blfiller@gmail.com>
2026-01-12 14:48:16 -0600

    fmod update for Dragon Kick and Asuran Fists

--- scripts/actions/weaponskills/asuran_fists.lua
+++ scripts/actions/weaponskills/asuran_fists.lua
@@ -25,8 +25,8 @@ weaponskillObject.onUseWeaponSkill = function(player, target, wsID, tp, primary,
 
     if xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.multiHitfTP = true -- http://wiki.ffo.jp/html/2424.html
-        params.str_wsc = 0.15 params.vit_wsc = 0.15
-        params.ftpMod = { 1.3, 1.3, 1.3 }
+        params.str_wsc = 0.5 params.vit_wsc = 0.5
+        params.ftpMod = { 1.0, 1.0, 1.0 }
     end
 
     local damage, criticalHit, tpHits, extraHits = xi.weaponskills.doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
