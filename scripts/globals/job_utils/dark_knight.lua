293c7be2e9b407c704c649dd650841767aa4b171
FiniteGamer17 <caefiller@gmail.com>
2026-01-11 18:47:26 -0600

    nether void balance change 2

--- scripts/globals/job_utils/dark_knight.lua
+++ scripts/globals/job_utils/dark_knight.lua
@@ -121,11 +121,11 @@ xi.job_utils.dark_knight.useLastResort = function(player, target, ability)
 end
 
 xi.job_utils.dark_knight.useNetherVoid = function(player, target, ability)
-    local power    = player:getHP() / 8
+    local power    = player:getHP() / 4
     local random   = 1 + math.random(5, 15) / 100
     local damage   = (power * 2) * random
 
-    player:setMP(player:getMP() + power * 2)
+    player:setMP(player:getMP() + power)
     player:setHP(player:getHP() - power)
 
     damage = utils.handleStoneskin(target, damage)
