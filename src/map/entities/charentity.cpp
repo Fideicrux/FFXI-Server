e521c94587adeec7a33ebf60d9787ad11db85744
Brandon Filler <blfiller@gmail.com>
2026-01-14 02:07:58 -0600

    double and triple attack fix

--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -1962,11 +1962,12 @@ void CCharEntity::OnRangedAttack(CRangeState& state, action_t& action)
     {
         hitCount += battleutils::getBarrageShotCount(this);
     }
-    else if (this->StatusEffectContainer->HasStatusEffect(EFFECT_TRIPLE_SHOT) && xirand::GetRandomNumber(100) < (40 + this->getMod(Mod::TRIPLE_SHOT_RATE)))
+
+    else if (xirand::GetRandomNumber(100) < this->getMod(Mod::TRIPLE_SHOT_RATE))
     {
         hitCount = 3;
     }
-    else if (this->StatusEffectContainer->HasStatusEffect(EFFECT_DOUBLE_SHOT) && xirand::GetRandomNumber(100) < (40 + this->getMod(Mod::DOUBLE_SHOT_RATE)))
+    else if (xirand::GetRandomNumber(100) < this->getMod(Mod::DOUBLE_SHOT_RATE))
     {
         hitCount = 2;
     }
