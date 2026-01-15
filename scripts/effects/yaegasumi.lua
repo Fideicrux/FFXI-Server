75f495255df8a20a329913188e07c3752b15c5d6
FiniteGamer17 <caefiller@gmail.com>
2026-01-14 19:32:12 -0600

    sam 2hr 2 fixed

--- scripts/effects/yaegasumi.lua
+++ scripts/effects/yaegasumi.lua
@@ -6,7 +6,7 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     effect:addMod(xi.mod.ALL_WSDMG_ALL_HITS, 50)
-    effect:addMod(xi.mod.SPECIAL_ATTACK_EVASION, 100)
+    effect:addMod(xi.mod.SPECIAL_ATTACK_EVASION, 1000)
 end
 
 effectObject.onEffectTick = function(target, effect)
