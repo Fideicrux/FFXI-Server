1e5a4daa09fb7cb4b1972776ca77fab7f3d03a6f
FiniteGamer17 <caefiller@gmail.com>
2026-01-17 23:49:16 -0600

    Ninja boomy

--- scripts/globals/job_utils/ninja.lua
+++ scripts/globals/job_utils/ninja.lua
@@ -57,7 +57,7 @@ xi.job_utils.ninja.useMijinGakure = function(player, target, ability, action)
     target:takeDamage(dmg, player, xi.attackType.SPECIAL, xi.damageType.ELEMENTAL)
     player:setLocalVar('MijinGakure', 1)
     player:setHP(1)
-    player:addStatusEffect(xi.effect.UTSUSEMI_SAN, 6, 0, 360)
+    player:addStatusEffect(xi.effect.COPY_IMAGE, 0, 0, 360, 0, 6)
 
     if player:hasStatusEffect(xi.effect.WEAKNESS) then
         player:delStatusEffect(xi.effect.WEAKNESS)
