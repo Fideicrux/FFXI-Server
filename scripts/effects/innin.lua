030b418db4d2b310e3d924fcd0316ac3253c5427
FiniteGamer17 <caefiller@gmail.com>
2026-01-18 04:35:51 -0600

    Innin FC test

--- scripts/effects/innin.lua
+++ scripts/effects/innin.lua
@@ -5,13 +5,14 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect) -- Power = 30 initially, subpower = 20 for enmity
-    local power   = effect:getPower()
+    local power    = effect:getPower()
+    local subpower = effect:getSubPower()
     local jpValue = target:getJobPointLevel(xi.jp.INNIN_EFFECT)
 
     target:addMod(xi.mod.NIN_NUKE_BONUS_INNIN, power)
-    target:addMod(xi.mod.ENMITY, -effect:getSubPower())
-    target:addMod(xi.mod.FASTCAST, effect:getPower())
-    target:addMod(xi.mod.CRIT_DMG_INCREASE, effect:getSubPower())
+    target:addMod(xi.mod.ENMITY, -subpower)
+    target:addMod(xi.mod.FASTCAST, 50)
+    target:addMod(xi.mod.CRIT_DMG_INCREASE, subpower)
     target:addMod(xi.mod.ACC, jpValue)
 end
 
@@ -21,13 +22,14 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     local power   = effect:getPower()
+    local subpower = effect:getSubPower()
     local jpValue = target:getJobPointLevel(xi.jp.INNIN_EFFECT)
 
     -- Remove the remaining modifiers.
     target:delMod(xi.mod.NIN_NUKE_BONUS_INNIN, power)
-    target:delMod(xi.mod.ENMITY, -effect:getSubPower())
-    target:delMod(xi.mod.FASTCAST, effect:getPower())
-    target:delMod(xi.mod.CRIT_DMG_INCREASE, effect:getSubPower())
+    target:delMod(xi.mod.ENMITY, -subpower)
+    target:delMod(xi.mod.FASTCAST, 50)
+    target:delMod(xi.mod.CRIT_DMG_INCREASE, subpower)
     target:delMod(xi.mod.ACC, jpValue)
 end
 
