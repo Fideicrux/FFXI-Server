85ec0824a09b176afa0cfe8f8804f20edb20174e
FiniteGamer17 <caefiller@gmail.com>
2026-01-18 04:37:40 -0600

    yonin follow up

--- scripts/effects/yonin.lua
+++ scripts/effects/yonin.lua
@@ -7,7 +7,7 @@ local effectObject = {}
 effectObject.onEffectGain = function(target, effect) -- power = 30 initially
     target:addMod(xi.mod.NINJA_TOOL, effect:getPower())
     target:addMod(xi.mod.ENMITY, effect:getPower())
-    target:addMod(xi.mod.FASTCAST, effect:getPower())
+    target:addMod(xi.mod.FASTCAST, 50)
 
     local yoninMerits = target:getMerit(xi.merit.YONIN_EFFECT)
     if yoninMerits ~= 0 then
@@ -26,7 +26,7 @@ effectObject.onEffectLose = function(target, effect)
     --remove the remaining power
     target:delMod(xi.mod.NINJA_TOOL, effect:getPower())
     target:delMod(xi.mod.ENMITY, effect:getPower())
-    target:delMod(xi.mod.FASTCAST, effect:getPower())
+    target:delMod(xi.mod.FASTCAST, 50)
 
     local yoninMerits = target:getMerit(xi.merit.YONIN_EFFECT)
     if yoninMerits ~= 0 then
