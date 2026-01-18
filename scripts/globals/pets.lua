995fc4d4ce36d2202b4b6d8ab688e39b0313610c
FiniteGamer17 <caefiller@gmail.com>
2026-01-18 01:45:09 -0600

    goldilocks

--- scripts/globals/pets.lua
+++ scripts/globals/pets.lua
@@ -116,10 +116,11 @@ xi.pet.spawnPet = function(caster, petID, state, target)
                 end)
             end
         elseif petID == xi.petId.ODIN then
-            local pet = caster:getPet()
+            local target = caster:getTarget()
+            local pet    = caster:getPet()
             if pet then
                 pet:timer(5000, function()
-                    pet:usePetAbility(xi.jobAbility.ZANTETSUKEN, caster:getTarget())
+                    pet:usePetAbility(xi.jobAbility.ZANTETSUKEN, target)
                 end)
             end
         end
