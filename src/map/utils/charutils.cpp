d78d8a2a13b80d3817c1b4c647e80ff00f691d99
FiniteGamer17 <caefiller@gmail.com>
2026-01-18 03:38:31 -0600

    job points

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -5153,20 +5153,20 @@ void DistributeCapacityPoints(CCharEntity* PChar, CMobEntity* PMob)
                 return;
             }
 
-            if (!hasKeyItem(PMember, KeyItem::JOB_BREAKER) || PMember->GetMLevel() < 99)
+            if PMember->GetMLevel() < 75)
             {
                 // Do not grant Capacity points without Job Breaker or Level 99
                 return;
             }
 
             bool  chainActive = false;
-            int16 levelDiff   = mobLevel - 99; // Passed previous 99 check, no need to calculate
+            int16 levelDiff   = mobLevel - 75; // Passed previous 99 check, no need to calculate
 
             // Capacity Chains are only granted for Mobs level 100+
             // Ref: https://www.bg-wiki.com/ffxi/Job_Points
             float capacityPoints = 0;
 
-            if (mobLevel > 99)
+            if (mobLevel > 90)
             {
                 // Base Capacity Point formula derived from the table located at:
                 // https://ffxiclopedia.fandom.com/wiki/Job_Points#Capacity_Points
