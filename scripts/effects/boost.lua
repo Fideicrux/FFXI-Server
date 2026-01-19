4766736033f9267545c205bfe4a8e554cf6901e3
FiniteGamer17 <caefiller@gmail.com>
2026-01-19 05:01:01 -0600

    BOOST better?

--- scripts/effects/boost.lua
+++ scripts/effects/boost.lua
@@ -8,7 +8,9 @@ effectObject.onEffectGain = function(target, effect)
 
     if effect:getSubPower() == 1 and target:isMob() then -- TODO: what happens if a player steals this boost? It has been observed that Yovra boost does nothing on retail but unknown if that is a bug or not.
         local baseDamage = target:getWeaponDmg()
-        effect:addMod(xi.mod.MAIN_DMG_RATING, (baseDamage * 1.5))
+        effect:addMod(xi.mod. POWER_MULTIPLIER_BASIC_ATTACK, 25)
+        effect:addMod(xi.mod. POWER_MULTIPLIER_WEAPONSKILL, 25)
+        effect:addMod(xi.mod. POWER_MULTIPLIER_JOB_ABILITY, 25)
     else
         effect:addMod(xi.mod.ATTP, effect:getPower())
     end
