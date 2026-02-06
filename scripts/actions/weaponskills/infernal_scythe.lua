ed114a7944f8dc57b170ba02f845b28485ce0853
FiniteGamer17 <caefiller@gmail.com>
2026-02-06 03:12:24 -0600

    scythe atk tuning

--- scripts/actions/weaponskills/infernal_scythe.lua
+++ scripts/actions/weaponskills/infernal_scythe.lua
@@ -24,7 +24,7 @@ weaponskillObject.onUseWeaponSkill = function(player, target, wsID, tp, primary,
     params.ele        = xi.element.DARK
     params.skill      = xi.skill.SCYTHE
     params.includemab = true
-    params.atkVaries = { 1.5, 1.5, 1.5 }
+    params.atkVaries = { 1.25, 1.25, 1.25 }
 
     if xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.multiHitFtp = true
