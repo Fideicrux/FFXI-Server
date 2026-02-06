462a069d6def2cafbd2a1ea51e804920ccfef3fb
FiniteGamer17 <caefiller@gmail.com>
2026-02-06 00:06:25 -0600

    ws tuning

--- scripts/actions/weaponskills/sickle_moon.lua
+++ scripts/actions/weaponskills/sickle_moon.lua
@@ -23,7 +23,7 @@ weaponskillObject.onUseWeaponSkill = function(player, target, wsID, tp, primary,
         params.accVaries = { 1.0, 1.25, 1.5 }
         params.atkVaries = { 1.25, 1.25, 1.25 }
         params.critVaries = { 0.1, 0.1, 0.1 } -- unless crit varies with TP.
-        params.str_wsc = 1.0
+        params.str_wsc = 0.6
         params.ftpMod = { 1.75, 3.5, 5.25 }
     end
 
